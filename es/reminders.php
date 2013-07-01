fabd7985b80b96af88abab2527ca3e52d21dce41
Nicolas Daniel Palumbo <npalumbo@xinax.net>
2013-07-01 18:40:01 +0200

    Directorio de locales por defecto en castellano Spanish default locale folder

---
+++ es/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Las contraseñas deben tener 6 caracteres y ser iguales.",
+
+	"user"     => "No se pundo encontrar a ningún usuario con ese e-mail.",
+
+	"token"    => "La clave de reestablecimiento de contraseña es inválida.",
+
+);
